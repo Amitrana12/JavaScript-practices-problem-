cdd08a244080872e0c11d1f8489570de198b153d
amit <[amirana14325@gmail.com]>
2021-04-26 19:15:59 +0530

    [Amit Rana]Added:: edit contact by pasing first name

--- Day 35/AddressBook.js
+++ Day 35/AddressBook.js
@@ -1,97 +1,116 @@
 //class
-class Address{
+class Address {
 
     //constructor
-constructor(...params ){
-    this.f_name=params[0];
-    this.l_name=params[1];
-    this.address=params[2];
-    this.city=params[3];
-    this.state=params[4];
-    this.zip=params[5];
-    this.phoneNumber=params[6];
-    this.email=params[7];
-    
-}
-//first name validation 
-get f_name(){return this._f_name}
-set f_name(f_name){
-    let f_nameregx=RegExp('^[A-Z]{1}[a-z]{3,}$')
-    if (f_nameregx.test(f_name))
-    this._f_name=f_name;
-    else throw ' first name is incorrect';
-}
-//laste name validaion
-get l_name(){return this._l_name}
-set l_name(l_name){
-    let l_nameregx=RegExp('^[A-Z]{1}[a-z]{3,}$')
-    if (l_nameregx.test(l_name))
-    this._l_name=l_name;
-    else throw ' last name is incorrect';
-}
-//address validation
-get address(){return this._address}
-set address(address){
-    let addressregx=RegExp('^[A-Za-z" "]{4,}$')
-    if (addressregx.test(address))
-    this._address=address;
-    else throw ' address is incorrect';
-}
-//city validation
-get city(){return this._city}
-set city(city){
-    let cityregx=RegExp('^[A-Za-z" "]{4,}$')
-    if (cityregx.test(city))
-    this._city=city;
-    else throw ' city is incorrect';
-}
-//state validation
-get state(){return this._state}
-set state(state){
-    let stateregx=RegExp('^[A-Za-z" "]{2,}$')
-    if (stateregx.test(state))
-    this._state=state;
-    else throw ' state is incorrect';
-}
-//zip validation
-get zip(){return this._zip}
-set zip(zip){
-    let zipregx=RegExp('^[0-9]{6,8}$');
-    if (zipregx.test(zip))
-    this._zip=zip;
-    else throw ' zip is incorrect';
-}
-//phone number validation
-get phoneNumber(){return this._phoneNumber}
-set phoneNumber(phoneNumber){
-    let phoneNumberregx=RegExp('^[0-9]{10,12}$');
-    if (phoneNumberregx.test(phoneNumber))
-    this._phoneNumber=phoneNumber;
-    else throw ' phone number is incorrect';
-}
-//eamil validation
-get email(){return this._email}
-set email(email){
-    let emailregx=RegExp('^[0-9A-Za-z]+([._+-][0-9A-Za-z]+)*[@][0-9A-Za-z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$');
-    if (emailregx.test(email))
-    this._email=email;
-    else throw ' email is incorrect';
-}
+    constructor(...params) {
+        this.f_name = params[0];
+        this.l_name = params[1];
+        this.address = params[2];
+        this.city = params[3];
+        this.state = params[4];
+        this.zip = params[5];
+        this.phoneNumber = params[6];
+        this.email = params[7];
 
-    getDetails(){
+    }
+    //first name validation 
+    get f_name() { return this._f_name }
+    set f_name(f_name) {
+        let f_nameregx = RegExp('^[A-Z]{1}[a-z]{3,}$')
+        if (f_nameregx.test(f_name))
+            this._f_name = f_name;
+        else throw ' first name is incorrect';
+    }
+    //laste name validaion
+    get l_name() { return this._l_name }
+    set l_name(l_name) {
+        let l_nameregx = RegExp('^[A-Z]{1}[a-z]{3,}$')
+        if (l_nameregx.test(l_name))
+            this._l_name = l_name;
+        else throw ' last name is incorrect';
+    }
+    //address validation
+    get address() { return this._address }
+    set address(address) {
+        let addressregx = RegExp('^[A-Za-z" "]{4,}$')
+        if (addressregx.test(address))
+            this._address = address;
+        else throw ' address is incorrect';
+    }
+    //city validation
+    get city() { return this._city }
+    set city(city) {
+        let cityregx = RegExp('^[A-Za-z" "]{4,}$')
+        if (cityregx.test(city))
+            this._city = city;
+        else throw ' city is incorrect';
+    }
+    //state validation
+    get state() { return this._state }
+    set state(state) {
+        let stateregx = RegExp('^[A-Za-z" "]{2,}$')
+        if (stateregx.test(state))
+            this._state = state;
+        else throw ' state is incorrect';
+    }
+    //zip validation
+    get zip() { return this._zip }
+    set zip(zip) {
+        let zipregx = RegExp('^[0-9]{6,8}$');
+        if (zipregx.test(zip))
+            this._zip = zip;
+        else throw ' zip is incorrect';
+    }
+    //phone number validation
+    get phoneNumber() { return this._phoneNumber }
+    set phoneNumber(phoneNumber) {
+        let phoneNumberregx = RegExp('^[0-9]{10,12}$');
+        if (phoneNumberregx.test(phoneNumber))
+            this._phoneNumber = phoneNumber;
+        else throw ' phone number is incorrect';
+    }
+    //eamil validation
+    get email() { return this._email }
+    set email(email) {
+        let emailregx = RegExp('^[0-9A-Za-z]+([._+-][0-9A-Za-z]+)*[@][0-9A-Za-z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$');
+        if (emailregx.test(email))
+            this._email = email;
+        else throw ' email is incorrect';
+    }
+    editcontact() {
+        let val = "Amit";
+        if (val == this.f_name) {
+            this.f_name = "Aman";
+            return true;
+        }
+    }
+//print all contact
+    getDetails() {
         return (`First name:: ${this.f_name},\nlast name::${this.l_name}, \naddress::${this.address}, \nCity name::${this.city}, \nState name::${this.state},\nzip ::${this.zip}, \nPhone NUmber::${this.phoneNumber}, \nemail::${this.email} `)
     }
 }
 console.log("Welcome to my Address book by [AMIT RANA] \n-------------------------\n");
-let arry=[];
-try{
+let arry = [];
+try {
     //adding new contact into address book by creatting arry
-arry[0]=new Address("Amit","Rana", "Awas vikas", "rishikesh", "UK", 247615, 8979325434, "amirana14325@gmail.com");
-arry[1]=new Address("Sumit","Sewal", "vikas", "rishikesh", "UK", 247615, 8979325434, "sumit@gmail.com");
-arry.forEach(function(data){
-console.log("---------------------\n"+data.getDetails());
-});}
-catch(e){
+    arry[0] = new Address("Amit", "Rana", "Awas vikas", "rishikesh", "UK", 247615, 8979325434, "amirana14325@gmail.com");
+    arry[1] = new Address("Sumit", "Sewal", "vikas", "rishikesh", "UK", 247615, 8979325434, "sumit@gmail.com");
+    arry.forEach(function (data) {
+        console.log("---------------------\n" + data.getDetails());
+    });
+    arry.forEach(function (element) {
+       
+            let val1 = element.editcontact();
+            if (val1 == true) {
+                console.log("----name updated----");
+
+            }
+    });
+    arry.forEach(function (data) {
+        console.log("---------------------\n" + data.getDetails());
+    });
+}
+catch (e) {
     console.log(e);
 }
 
