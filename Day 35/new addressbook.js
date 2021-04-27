a6bba90ffce27359c913805f54a73afa88677dbc
amit <[amirana14325@gmail.com]>
2021-04-27 08:25:36 +0530

    [Amit Rana]Added::view contact by city and state name

--- Day 35/new addressbook.js
+++ Day 35/new addressbook.js
@@ -216,4 +216,14 @@ function searchbyCityOrState() {
     addressBookArray.forEach(contact => console.log(addressBookArray));
 }
 searchbyCityOrState();
-
+//UC9-----------------------------------------------------------------------
+function viewByCityOrState() {
+    let inputCity = prompt("Enter city to search person : ");
+    let inputState = prompt("Enter state to search person : ");
+    if(addressBookArray.some(contact => contact.city == inputCity && contact.state == inputState)) {
+        console.log("Person in city " +inputCity+ " and state " +inputState);
+        }
+        addressBookArray.filter(contact => contact.city == inputCity && contact.state == inputState)
+        addressBookArray.forEach(contact => console.log(addressBookArray));
+}
+viewByCityOrState();
